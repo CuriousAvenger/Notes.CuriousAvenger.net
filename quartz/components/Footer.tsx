e6557debcb18e1058d2edbb69510f3e04e501aaf
CuriousAvenger <sai.praneth10@gmail.com>
2025-02-22 15:18:48 -0800

    fixed footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,33 +1,27 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import {QuartzComponent, QuartzComponentConstructor, QuartzComponentProps} from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
-    const links = opts?.links ?? []
-    return (
-      <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
-      </footer>
-    )
-  }
+    const quartz = "https://github.com/jackyzha0/quartz"
+    const ccm = "https://github.com/CuriousAvenger/Notes.CuriousAvenger.net"
+    const linkd = "https://www.linkedin.com/in/sai-praneth-raju/"
+    
+    const Footer: QuartzComponent = (props: QuartzComponentProps) => {
+        const year = new Date().getFullYear()
+        return (
+            <footer>
+                <p>Powered By <a href={quartz} class="external">Quartz</a> © {year} | 
+                  <a href={ccm} class="external"> GitHub</a> - Notes.CuriousAvenger.net | 
+                  <a href={linkd} class="external"> LinkedIn</a> - Sai Praneth
+                </p>
+            </footer>
+        )
+    }
 
-  Footer.css = style
-  return Footer
+    Footer.css = style
+    return Footer
 }) satisfies QuartzComponentConstructor
